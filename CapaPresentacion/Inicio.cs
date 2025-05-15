42d1aa9db1fa4f6a95288229481382ead86aa825
MarcoR <marcoreyes2027@gmail.com>
2025-05-14 19:07:53 -0500

    Agregar archivos de proyecto.

---
+++ CapaPresentacion/Inicio.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CapaEntidad;
+using FontAwesome.Sharp;
+using CapaNegocio;
+
+namespace CapaPresentacion
+{
+    public partial class Inicio : Form
+    {
+
+        private static Usuario usuarioActual;
+        private static IconMenuItem menuActivo = null;
+        private static Form formularioActivo = null;
+
+        public Inicio(Usuario objusuario)
+        {
+            usuarioActual = objusuario;                      
+
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Inicio_Load(object sender, EventArgs e)
+        {
+            List<Permiso> ListaPermisos = new CN_Permiso().Listar(usuarioActual.IdUsuario);
+
+            foreach (IconMenuItem iconMenu in menu.Items)
+            {
+                bool encontrado = ListaPermisos.Any(m => m.NombreMenu == iconMenu.Name);
+
+                if (encontrado == false)
+                {
+                    iconMenu.Visible = false;
+                }
+            }
+
+            lblusuario.Text = usuarioActual.NombreCompleto;
+        }
+
+        private void lblusuario_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void abrirFormulario(IconMenuItem menu, Form formulario)
+        {
+            if (menuActivo != null)
+            {
+                menuActivo.BackColor = Color.White;
+            }
+
+            menu.BackColor = Color.Silver;
+            menuActivo = menu;
+
+            if(formularioActivo != null)
+            {
+                formularioActivo.Close();
+                formulario.TopLevel = false;
+                formulario.FormBorderStyle = FormBorderStyle.None;
+                formulario.Dock = DockStyle.Fill;
+                formulario.BackColor = Color.SteelBlue;
+
+                contenedor.Controls.Add(formulario);
+                formulario.Show();
+            }
+
+            formularioActivo = formulario;
+
+        }
+
+        private void menuusuarios_Click(object sender, EventArgs e)
+        {
+            abrirFormulario((IconMenuItem)sender, new fmrUsuarios());
+
+        }
+
+        private void contenedor_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void submenucategoria_Click(object sender, EventArgs e)
+        {
+            abrirFormulario(menumantenedor, new frmCategoria());
+        }
+
+        private void subMenuProducto_Click(object sender, EventArgs e)
+        {
+            abrirFormulario(menumantenedor, new frmProducto());
+        }
+
+        private void subMenuRegistrarVenta_Click(object sender, EventArgs e)
+        {
+            abrirFormulario(menuventas, new frmVentas());
+        }
+
+        private void subMenuVerDetalleVenta_Click(object sender, EventArgs e)
+        {
+            abrirFormulario(menuventas, new frmDetalleVenta());
+        }
+
+        private void subMenuRegistrarCompra_Click(object sender, EventArgs e)
+        {
+            abrirFormulario(menucompras, new frmCompras());
+        }
+
+        private void subMenuVerDetalleCompra_Click(object sender, EventArgs e)
+        {
+            abrirFormulario(menucompras, new frmDetalleCompra());
+        }
+
+        private void menuclientes_Click(object sender, EventArgs e)
+        {
+            abrirFormulario((IconMenuItem)sender, new frmClientes());
+        }
+
+        private void menuproveedores_Click(object sender, EventArgs e)
+        {
+            abrirFormulario((IconMenuItem)sender, new frmProveedores());
+        }
+
+        private void menureportes_Click(object sender, EventArgs e)
+        {
+            abrirFormulario((IconMenuItem)sender, new frmReportes());
+        }
+    }
+}
